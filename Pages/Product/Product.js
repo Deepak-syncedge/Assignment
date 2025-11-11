f58eead80a0f853521a11b5c946bbf2d21e98909
Deepak-syncedge <Deepak213@syncedge>
2025-11-11 07:41:10 +0530

    Deleted folder

--- Pages/Product/Product.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelectorAll('.addtocart');
-
-button.forEach(button => {
-    button.addEventListener('click', function() {
-    alert('Added to cart!');
-    console.log('Button clicked');
-    }
-    )
-});
